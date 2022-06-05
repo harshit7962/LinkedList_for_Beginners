6c6708119d885deb48265768c2a04959279d5455
Harshit <62823486+harshit7962@users.noreply.github.com>
2022-06-05 18:54:27 +0530

    Create dequeue.cpp

---
+++ DoubleLL/dequeue.cpp
@@ -0,0 +1,72 @@
+struct Node {
+    int val;
+    Node *prev;
+    Node *next;
+    Node(int x) {
+        val = x;
+        prev = NULL;
+        next = NULL;
+    }
+};
+
+struct Dequeue {
+    int size;
+    Node *front;
+    Node *rear;
+    
+    Dequeue() {
+        front = NULL;
+        rear = NULL;
+        size=0;
+    }
+    
+    void insertFront(int x) {
+        Node *temp = new Node(x);
+        if(front==NULL) {rear=temp; front=temp;}
+        else {
+            temp->next = front;
+            front->prev = temp;
+            front = temp;
+        }
+        size++;
+    }
+    
+    void insertRear(int x) {
+        Node *temp = new Node(x);
+        if(rear==NULL) {front=temp;rear=temp;}
+        else{
+            temp->prev = rear;
+            rear->next = temp;
+            rear = temp;
+        }
+        size++;
+    }
+    
+    int getFront() {
+        return front->val;
+    }
+    
+    int getRear(){
+        return rear->val;
+    }
+    
+    void deleteFront(){
+        if(size==0) return;
+        Node *temp = front;
+        front = front->next;
+        if(front==NULL) rear=NULL;
+        else front->prev = NULL;
+        delete temp;
+    }
+    
+    void deleteRear() {
+        if(size==0) return;
+        Node *temp = rear;
+        rear = rear->prev;
+        if(rear==NULL) front = NULL;
+        else rear->next =NULL;
+        delete temp;
+    }
+    
+    bool isEmpty() {return size==0;}
+};
